fe06ad82c78344099615df5690e5a37551818e31
eduardoscovino <eduardo.scovino@gmail.com>
2021-02-19 19:21:07 -0300

    relacionando o repo com o csv

--- livro.rb
+++ livro.rb
@@ -1,11 +1,11 @@
-class Livro {
+class Livro
   attr_reader :id, :titulo, :autor, :avaliacao
   attr_accessor :preco
   def initialize(id, titulo, autor, avaliacao, preco)
-    @id =         id,
-    @titulo =     titulo,
-    @autor =      autor,
-    @avaliacao =  avaliacao,
+    @id =         id
+    @titulo =     titulo
+    @autor =      autor
+    @avaliacao =  avaliacao
     @preco =      preco
   end
-}
+end
